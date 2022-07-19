0e83fec2307b925b266776a72c9440d426f38fcc
Morev Igor <mo.ig2017@yandex.ru>
2022-07-19 11:51:21 +0500

    Creat program

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 1005;
+        int divider = 20;
+        int bonus;
+
+        bonus = cost / divider;
+        System.out.println("Бесплатные мили:" + bonus);
+
+
+    }
+}
